d5e8ee703c61ffab95ceed58e5bbc3c1a453d83f
Alex <jalexlopez15@gmail.com>
2021-05-25 01:24:23 -0700

    changed qty pricing table and corrected receipt data

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
 const preWorkoutPrice = 10;
-const proteinPrice = 45.50;
 const taxRate = 0.08;
-
 var date = new Date();
-
-var today = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
-
-var shippingValue;
-
+var today = date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear();
 var quantity = [10, 50, 100, 200, 500, 1000];
-
 var discountPercent = [.05, .07, .1, .12, .15, .2];
-
 var percentArrayLength = discountPercent.length;
-
 var numQuantity = quantity.length;
-
 var discountAmount,
     discountFound,
     discountApplied,
     normalPricing,
     discountPricing,
-    strDiscountPricing;
+    strDiscountPricing,
+    shippingValue;
 
-/* Makes shipping selection from radio buttons*/
+
+
+/*Makes shipping selection from radio buttons*/
 function getRadioSelection() {
-
     var selection = document.getElementsByTagName('input');
+    shippingValue = document.getElementsByName(shippingValue).value;
     for (i = 0; i < selection.length; i++) {
         if (selection[i].checked) {
             shippingValue = selection[i].value;
@@ -34,20 +27,29 @@ function getRadioSelection() {
     }
 }
 
+/*Toggle discount pricing table*/
+function toggleDiscountTable() {
+    var x = document.getElementById('qtyPricing');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
 /* Calculates discount pricing based on quantity ordered by user*/
 function calculateDiscount() {
     let i = 0;
     let found = false;
-
     while (i < numQuantity && !found) {
-        if (txtQ1 < quantity[0]) {
+        if (prodQuantity < quantity[0]) {
             discountApplied = 0;
             found = true;
-        } else if (txtQ1 >= quantity[i] && txtQ1 < quantity[i + 1]) {
+        } else if (prodQuantity >= quantity[i] && prodQuantity < quantity[i + 1]) {
             discountFound = discountPercent[i];
             discountApplied = discountFound * 100;
             found = true;
-        } else if (txtQ1 >= quantity[5]) {
+        } else if (prodQuantity >= quantity[5]) {
             discountFound = discountPercent[5];
             discountApplied = discountFound * 100;
             found = true;
@@ -55,98 +57,53 @@ function calculateDiscount() {
             i++;
         }
     }
-    if (txtQ1 >= 10) {
-        normalPricing = preWorkoutPrice * txtQ1;
+    if (prodQuantity >= 10) {
+        normalPricing = preWorkoutPrice * prodQuantity;
         discountAmount = discountFound * normalPricing;
         discountPricing = normalPricing - discountAmount;
     } else {
-        discountPricing = preWorkoutPrice * txtQ1;
+        discountPricing = preWorkoutPrice * prodQuantity;
     }
-
-
 }
 
 /*-------------- Renders receipt ----------------*/
-
 function processOrder() {
-
-
-
     txtName = document.getElementById('txtName').value;
-    txtQ1 = document.getElementById('txtQ1').value;
-    txtQ2 = document.getElementById('txtQ2').value;
-
-    var preworkoutTotal = preWorkoutPrice * txtQ1;
-    var proteinTotal = proteinPrice * txtQ2;
-    var subtotal = preworkoutTotal + proteinTotal;
-    var tax = subtotal * taxRate;
-    var balance = tax + subtotal;
+    prodQuantity = document.getElementById('prodQuantity').value;
 
     calculateDiscount();
     getRadioSelection();
+
+    var preworkoutTotal = preWorkoutPrice * prodQuantity;
+    var subtotal = discountPricing;
+    var tax = subtotal * taxRate;
+    var balance = tax + subtotal;
+
+
     if (discountApplied > 0) {
         document.getElementById('requiredMsg').innerHTML = '';
-        document.getElementById('receipt').innerHTML = txtName + ', thank you for your order of ' + txtQ1 + ' pre workouts and ' + txtQ2 + ' protein tubs!' + "<br/>" + txtQ1 + ' pre workouts @' + preWorkoutPrice.toFixed(2) + ' each = $' + discountPricing.toFixed(2) + '**quantity discount ' + discountApplied.toFixed(0) + '% applied**' + "<br/>" + txtQ2 + ' protein tubs @' + proteinPrice.toFixed(2) + ' each = $' + (proteinTotal.toFixed(2)) + "<br/>" + 'Subtotal: $' + subtotal.toFixed(2) + "<br/>" + 'Tax: $' + tax.toFixed(2) + "<br/>" + 'Shipping Fee: $' + shippingValue + "<br/>" + 'Total Balance: $' + balance.toFixed(2) + "<br/>" + today;
+        document.getElementById('receipt').innerHTML = txtName + ', thank you for your order of ' + prodQuantity + ' pre workouts!' + "<br/>" + prodQuantity + ' pre workouts - $' + preWorkoutPrice.toFixed(2) + ' each = $' + preworkoutTotal.toFixed(2) + "<br/>" + 'Subtotal: $' + subtotal.toFixed(2) + '**quantity discount ' + discountApplied.toFixed(0) + '% applied**' + "<br/>" + 'Tax: $' + tax.toFixed(2) + "<br/>" + 'Shipping Fee: $' + shippingValue + "<br/>" + 'Total Balance: $' + balance.toFixed(2) + ' + shipping' + "<br/>" + today;
     } else {
         document.getElementById('requiredMsg').innerHTML = '';
-        document.getElementById('receipt').innerHTML = txtName + ', thank you for your order of ' + txtQ1 + ' pre workouts and ' + txtQ2 + ' protein tubs!' + "<br/>" + txtQ1 + ' pre workouts @' + preWorkoutPrice.toFixed(2) + ' each = $' + discountPricing.toFixed(2) + "<br/>" + txtQ2 + ' protein tubs @' + proteinPrice.toFixed(2) + ' each = $' + (proteinTotal.toFixed(2)) + "<br/>" + 'Subtotal: $' + subtotal.toFixed(2) + "<br/>" + 'Tax: $' + tax.toFixed(2) + "<br/>" + 'Shipping Fee: $' + shippingValue + "<br/>" + 'Total Balance: $' + balance.toFixed(2);
+        document.getElementById('receipt').innerHTML = txtName + ', thank you for your order of ' + prodQuantity + ' pre workouts!' + "<br/>" + prodQuantity + ' pre workouts - $' + preWorkoutPrice.toFixed(2) + ' each = $' + discountPricing.toFixed(2) + "<br/>" + 'Subtotal: $' + subtotal.toFixed(2) + "<br/>" + 'Tax: $' + tax.toFixed(2) + "<br/>" + 'Shipping Fee: $' + shippingValue + "<br/>" + 'Total Balance: $' + balance.toFixed(2) + ' + shipping' + "<br/>" + today;
     }
 
     /*----------Input validation---------*/
     let textboxes = document.getElementsByClassName('required');
-    let totQuantity = 0;
-    let i;
-    for (i = 0; i < textboxes.length; i++) {
-
+    let totalQuantity = 0;
+    for (let i = 0; i < textboxes.length; i++) {
         let quantity = parseInt(textboxes[i].value);
-
         if (quantity >= 0) {
-
-            totQuantity += quantity;
+            totalQuantity += quantity;
         } else if (textboxes[i].value < 0 || textboxes[i].value == "" || textboxes[i].value == "REQUIRED!") {
             document.getElementById('requiredMsg').innerHTML = "* indicates a required field";
             document.getElementById('receipt').innerHTML = "<span style='color: red;'>Order could not be processed due to missing information. Please fill out all required fields.</span>";
             document
             textboxes[i].value = 'REQUIRED!';
             textboxes[i].select();
-
         }
-
-
-
     }
-
-}
-
-
-
-/*------------- Discount Pricing Table -------------*/
-function qtyPricingTable() {
-
-    var heading = document.getElementById('heading').innerHTML = "Quantity Discount Pricing on Pre Workouts";
-
-    var strPricingTable = "<table><tr><th>Quantity</th><th>Normal Pricing</th><th>Discount Pricing</th></tr>";
-
-    for (var i = 0; i < numQuantity; i++) {
-
-        normalPricing = quantity[i] * preWorkoutPrice;
-
-        discountAmount = normalPricing * discountPercent[i];
-
-        discountPricing = normalPricing - discountAmount;
-
-        strDiscountPricing = "$" + discountPricing;
-
-        strPricingTable += '<tr><td>' + quantity[i] + '</td><td>' + '$' + normalPricing + '</td><td>' + strDiscountPricing + '</td></tr>';
-
-    }
-
-    strPricingTable += '</table>';
-
-    document.getElementById('displayQtyPricingTable').innerHTML = strPricingTable;
-
-    return;
 }
 
 document.getElementById("btnProcess").addEventListener("click", processOrder);
-document.getElementById("qtyPricing").addEventListener("click", qtyPricingTable);
+document.getElementById("discountPricingTable").addEventListener("click", toggleDiscountTable);
